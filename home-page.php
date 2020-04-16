056b116480e41d4a7fa43cff2d6dd4853a08c184
TahnayaM <61609153+TahnayaM@users.noreply.github.com>
2020-04-16 18:55:40 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -23,27 +23,47 @@
       <div class="container">
           <div class="row">
               
-          <div class="article-title text-center">
-              <h3>LATEST ARTICLES</h3>
-              </div>
-              
-              <div class="col-md-4 article-cent">
-                  
-                      
+       
+              <div class="article-title text-center">
+               <h3 class="text-center article-title "><?php the_field('titleforarticles'); ?></h3>
 
               </div>
               
-                   <div class="col-md-4 article-cent">
-                       
-               
+               <div class="col-md-4 article-cent">
+                    <img class="img-responsive" alt=“portait” src="<?php the_field('articleimage'); ?>">
+                    <h4>
+                        <?php the_field('articletitle'); ?>
+                    </h4>
+                    <p>
+                        <?php the_field('articleexcerpt'); ?>
+                    </p>
+                <a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
+                </div>
 
-              </div>
               
-                   <div class="col-md-4 article-cent">
-                       
-    
+           <div class="col-md-4 article-cent">
+                    <img class="img-responsive" alt=“portait” src="<?php the_field('articleimage2'); ?>">
+                    <h4>
+                        <?php the_field('articletitle2'); ?>
+                    </h4>
+                    <p>
+                        <?php the_field('articleexcerpt2'); ?>
+                    </p>
+                <a href="<?php the_field('readmore2'); ?>" class="readmore">CONTINUE READING</a>
+                </div>
 
-              </div>
+              
+                     <div class="col-md-4 article-cent">
+                    <img class="img-responsive" alt=“portait” src="<?php the_field('articleimage3'); ?>">
+                    <h4>
+                        <?php the_field('articletitle3'); ?>
+                    </h4>
+                    <p>
+                        <?php the_field('articleexcerpt3'); ?>
+                    </p>
+                <a href="<?php the_field('readmore3'); ?>" class="readmore">CONTINUE READING</a>
+                </div>
+
               
           </div><!--          row-->
           </div><!--           container-->
